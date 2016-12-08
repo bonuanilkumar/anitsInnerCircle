a7dcc60460c686a3963759a0b3a9dd43976f936b
bonu <bonuanilkumar@gmail.com>
2016-12-08 13:51:27 -0500

    Inital Commit

---
+++ src/main/java/com/anits/AnitsInnerCircleApplication.java
@@ -0,0 +1,12 @@
+package com.anits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnitsInnerCircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnitsInnerCircleApplication.class, args);
+	}
+}
